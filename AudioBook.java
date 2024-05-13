362bd6830976ef77bae12f7e176a7447c2f6ad5b
usmaarn <usmaarn@gmail.com>
2024-05-13 17:21:07 +0100

    new commmit

--- AudioBook.java
+++
@@ -1,9 +0,0 @@
-
-public class AudioBook extends Book {
-    private int runTime;
-
-    AudioBook(String title, String author, int runTime) {
-        super(title, author, 0);
-        this.runTime = runTime;
-    }
-}
--- Main.java
+++ Main.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
 
 class Main {
     public  static void main(String[] args){
-        Rabbit rabbit = new Rabbit();
+        Scanner scanner = new Scanner(System.in);
+        Animal animal;
 
-        rabbit.flee();
-        rabbit.fight();
+        System.out.print("What animal do you want? ");
+        System.out.print("1=Dog, 2=Cat: ");
+        int choice = scanner.nextInt();
+
+        if(choice == 1){
+            animal = new Dog();
+            animal.speak();
+        }
+
+        else if(choice == 2){
+            animal = new Cat();
+            animal.speak();
+        }
+
+        else {
+            animal = new Animal();
+            System.out.println("Invalid option");
+            animal.speak();
+        }
+
+        scanner.close();
     }
 }
