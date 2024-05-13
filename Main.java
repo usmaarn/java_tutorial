60a0bc835d35c9d32e1afeac149e4b4a47287e6c
usmaarn <usmaarn@gmail.com>
2024-05-13 17:25:52 +0100

    learned exception

--- Main.java
+++ Main.java
@@ -1,30 +1,37 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class Main {
     public  static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        Animal animal;
+        try{
+            Animal animal;
 
-        System.out.print("What animal do you want? ");
-        System.out.print("1=Dog, 2=Cat: ");
-        int choice = scanner.nextInt();
+            System.out.print("What animal do you want? ");
+            System.out.print("1=Dog, 2=Cat: ");
+            int choice = scanner.nextInt();
 
-        if(choice == 1){
-            animal = new Dog();
-            animal.speak();
+            if(choice == 1){
+                animal = new Dog();
+                animal.speak();
+            }
+
+            else if(choice == 2){
+                animal = new Cat();
+                animal.speak();
+            }
+
+            else {
+                animal = new Animal();
+                System.out.println("Invalid option");
+                animal.speak();
+            }
         }
-
-        else if(choice == 2){
-            animal = new Cat();
-            animal.speak();
+        catch (InputMismatchException e) {
+            System.out.println("Invalid Input");
         }
-
-        else {
-            animal = new Animal();
-            System.out.println("Invalid option");
-            animal.speak();
+        finally {
+            scanner.close();
         }
-
-        scanner.close();
     }
 }
